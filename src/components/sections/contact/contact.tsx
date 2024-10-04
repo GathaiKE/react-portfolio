5a38c1d28c29106633b9bf10503a08e38f7b4673
Brian Gathai <kariuki.gathai@thejitu.com>
2024-10-04 15:56:35 +0800

    FEAT: Contact form configured to send the message

--- src/components/sections/contact/contact.tsx
+++ src/components/sections/contact/contact.tsx
@@ -1,10 +1,47 @@
-import { Form } from 'react-router-dom'
+import { ActionFunctionArgs, Form } from 'react-router-dom'
 import './contact.css'
-import { socialApps } from '../../../data'
+import { environment, socialApps } from '../../../data'
 import { useEffect, useState } from 'react'
 import { Socials } from '../../../interfaces/interfaces'
 import SocialsBtn from '../../socialsBtn/socialsBtn'
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com'
 
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const action = async ({request}:ActionFunctionArgs)=>{
+    const formData = await request.formData()
+
+    const name:string = formData.get('name') as string
+    const email:string = formData.get('email') as string
+    const message:string = formData.get('message') as string
+
+    const service_id:string= environment.SERVICE_ID
+    const template_id:string = environment.TEMPLATE_ID
+    const user_id:string = environment.USER_ID
+
+    const data = {
+        from_name:name,
+        to_name: "Brian Gathai",
+        subject: "Portfolio contact",
+        message:message,
+        response_address:email
+    }
+
+    try {
+        const result:EmailJSResponseStatus = await emailjs.send(service_id, template_id, data,user_id)
+        return {
+            status:result.status,
+            statusText: result.text,
+            message:"Your message was received and will ve responded to as soon as possible"
+        }
+    } catch (error:unknown) {
+        return {
+            error:error,
+            status: 403,
+            message:"Failed to send the text"
+        }
+    }
+}
 
 const Contact:React.FC = ()=>{
 
