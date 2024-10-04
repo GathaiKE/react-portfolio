bdaee28cc68dfa5ca5b09a365d492ae4893d9488
Brian Gathai <kariuki.gathai@thejitu.com>
2024-10-04 20:24:04 +0800

    FEAT: mobile menu + hamburger created but not styled to completion

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-scroll';
 import './nav.css'
-import home from '../../assets/icons/home.png'
-import about from '../../assets/icons/about.png'
-import testimonial from '../../assets/icons/testimonial.png'
-import projects from '../../assets/icons/projects.png'
-import skills from '../../assets/icons/skills.png'
-import contact from '../../assets/icons/contact.png'
+import { useState } from 'react';
 
+const Navigation = () => {
+const [openMenu, setOpenMenu] = useState(false)
 
-const Navigation = ()=>{
+    const toggleMenu=()=>{
+        setOpenMenu(val=>!val)
+        console.log(openMenu)
+    }
+
     return (
         <>
-            <nav className='home-nav'>
+            <nav className='home-nav' >
                 <Link to="home" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>home</Link>
                 <Link to="about" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>about</Link>
                 <Link to="skills" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>my skills</Link>
@@ -19,26 +20,59 @@ const Navigation = ()=>{
                 <Link to="testimonials" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>testimonials</Link>
                 <Link to="contact" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>contact</Link>
             </nav>
+
             <nav className='mobile-nav'>
-                <Link to="home" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={home} alt="home" />
-                </Link>
-                <Link to="about" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={about} alt="about" />
-                </Link>
-                <Link to="skills" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={skills} alt="my skills" />
-                </Link>
-                <Link to="projects" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={projects} alt="projects" />
-                </Link>
-                <Link to="testimonials" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={testimonial} alt="testimonials" />
-                </Link>
-                <Link to="contact" smooth={true} className='home-nav--link' activeClass='.home-nav--link-active'>
-                    <img src={contact} alt="contact" />
-                </Link>
+                <div className={`hamburger ${openMenu?"open-burger":""}`} onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <span className={`menu ${openMenu?".menu-open":""}`}>
+                    <Link 
+                        to="home" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        home</Link>
+                    <Link 
+                        to="about" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        about</Link>
+                    <Link 
+                        to="skills" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        my skills</Link>
+                    <Link 
+                        to="projects" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        projects</Link>
+                    <Link 
+                        to="testimonials" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        testimonials</Link>
+                    <Link 
+                        to="contact" 
+                        smooth={true} 
+                        className='mobile-link'
+                        activeClass='mobile-link-active'
+                    >
+                        contact</Link>
+                </span>
             </nav>
+
         </>
     )
 }
